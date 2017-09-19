a2bf20c599aa758adb3c4eaaa557a2d46758e641
merkulovvs <mervic81@gmail.com>
2017-09-19 11:50:41 +0300

    Initial commit

---
+++ src/HomeWork1.java
@@ -0,0 +1,8 @@
+/**
+ * Created by merkulovvs on 9/19/2017.
+ */
+public class HomeWork1 {
+    public static void main(String[] args) {
+        System.out.println("Отдыхал на море, ездил в Карпаты");
+    }
+}
